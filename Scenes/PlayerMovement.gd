d101dcb7713cf7b1425a7999d77c5bcd414482be
SteeRevo <69092521+SteeRevo@users.noreply.github.com>
2022-05-09 20:28:26 -0700

    added some player movement

--- Scenes/PlayerMovement.gd
+++ Scenes/PlayerMovement.gd
@@ -1,14 +1,27 @@
 extends KinematicBody2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+const ACCELERATION = 512
+const MAX_SPEED = 64
+const FRICTION = 0.25
+const GRAVITY = 200
+const JUMP_FORCE = 120
+
+var motion = Vector2.ZERO
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func _physics_process(delta):
+	var x_in = Input.get_action_strength("right") - Input.get_action_strength("left")
+	
+	if x_in != 0:
+		motion.x += x_in * ACCELERATION * delta
+		motion.x = clamp(motion.x, -MAX_SPEED, MAX_SPEED)
+	
+	
+	motion.y += GRAVITY + delta
+	motion = move_and_slide(motion);
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
