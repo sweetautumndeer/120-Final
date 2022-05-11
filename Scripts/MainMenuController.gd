628c0831dbefce1ef4a8d4159e0fc97ffedb1eeb
sweetautumndeer <78242996+sweetautumndeer@users.noreply.github.com>
2022-05-11 15:44:33 -0700

    Fixed Duplicate MainMenu.tscn

--- Scripts/MainMenuController.gd
+++ Scripts/MainMenuController.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Button
 
 
 # Declare member variables here. Examples:
@@ -8,9 +8,8 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print("here")
 	pass
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+
+func _on_PlayButton_pressed():
+	get_tree().change_scene("res://Scenes/Level1.tscn")
