c798886ce04bd9be794e386f6d92adb581339345
sweetautumndeer <amoulios@yahoo.com>
2022-05-24 16:26:12 -0700

    Adjusted Bullet Speeds, Enemies Shoot At Player
    Enemies now shoot directly at the player. This makes the game really hard lol

--- Enemies/bulletSpawner.gd
+++ Enemies/bulletSpawner.gd
@@ -5,6 +5,7 @@ extends Position2D
 # var a = 2
 # var b = "text"
 var bullet = preload("res://Enemies/EnemyBullet.tscn")
+onready var player = get_node("../../Player");
 var can_fire = true;
 
 
@@ -15,9 +16,16 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
+	#aim at player
+	look_at(player.get_position())
+	
 	if can_fire:
 		var bullet_instance = bullet.instance()
 		bullet_instance.rotation = rotation
+		
+		#flips bullet if gun is flipped
+		if get_node("../AnimatedSprite").flip_v == true:
+			bullet_instance.get_node("Sprite").set_flip_v(false)
 		
 		#creates instance at gun position
 		bullet_instance.global_position = $".".global_position
@@ -25,5 +33,5 @@ func _physics_process(delta):
 		
 		#timer until gun can be fire again
 		can_fire = false
-		yield(get_tree().create_timer(4.0), "timeout")
+		yield(get_tree().create_timer(2.0), "timeout")
 		can_fire = true;
