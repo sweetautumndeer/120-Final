1fda0b62379d777d6fcfc5be8c4d1af3816fa2e9
sweetautumndeer <78242996+sweetautumndeer@users.noreply.github.com>
2022-05-27 16:21:36 -0700

    Added Retry/Main Menu Buttons to Lose Screen

--- LevelScenes/LoseScreen.gd
+++ LevelScenes/LoseScreen.gd
@@ -15,3 +15,11 @@ func _ready():
 func _process(delta):
 	if Input.get_action_strength("jump"):
 		get_tree().change_scene("res://LevelScenes/Level1.tscn")
+
+
+func _on_RetryButton_pressed():
+	get_tree().change_scene("res://LevelScenes/Level1.tscn")
+
+
+func _on_MainMenuButton_pressed():
+	get_tree().change_scene("res://LevelScenes/MainMenu.tscn")
