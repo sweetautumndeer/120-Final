eb3f16170b8a7bed1be17a31077d8a4221132e25
SteeRevo <69092521+SteeRevo@users.noreply.github.com>
2022-05-28 03:00:52 -0700

    added hitflash to player and enemy again

--- Enemies/Enemy.gd
+++ Enemies/Enemy.gd
@@ -12,6 +12,11 @@ export var enemy_health = 3
 var bullet = preload("res://Player/PlayerBullet.tscn")
 var fire_pause
 
+onready var hitflash = $AnimationPlayer
+
+func _ready():
+	hitflash.play("Stop")
+
 #can only fall, does not move
 func _physics_process(delta):
 	motion.y += GRAVITY * delta
@@ -29,6 +34,7 @@ func _on_Area2D_body_entered(body):
 #if area2D node enters enemy hurtbox, will damage health. once no more health, is deleted
 func _on_Area2D_area_entered(area):
 	enemy_health -= 1
+	hitflash.play("Start")
 	print(enemy_health)
 	if enemy_health == 0:
 		queue_free()
