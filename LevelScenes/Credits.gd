fba1cc4961bac48655a44f7e635f0d63f6e196e5
sweetautumndeer <amoulios@yahoo.com>
2022-06-01 12:46:32 -0700

    Added Timers to the Win/Credits Screens

--- LevelScenes/Credits.gd
+++ LevelScenes/Credits.gd
@@ -1,10 +1,18 @@
 extends Node2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-func _input(event):
-	if (event is InputEventKey and event.pressed) or (event is InputEventMouseButton and event.pressed):
-			get_tree().change_scene("res://LevelScenes/MainMenu.tscn")
+var maxTime = 2.5
+var t = 0;
 
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	t = 0
+
+func _process(delta):
+	t += delta
+	if t > maxTime:
+		$Continue.visible = true
+
+func _input(event):
+	if t > maxTime and ((event is InputEventKey and event.pressed) or (event is InputEventMouseButton and event.pressed)):
+		get_tree().change_scene("res://LevelScenes/MainMenu.tscn")
