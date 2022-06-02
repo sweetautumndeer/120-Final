64e3d86809326c3d24bf4ee3c9e2fb9b203dabb4
sweetautumndeer <amoulios@yahoo.com>
2022-06-02 12:35:36 -0700

    Tested an in-game audio mixer

--- LevelScenes/Options.gd
+++ LevelScenes/Options.gd
@@ -4,14 +4,21 @@ extends Node2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-#onready var healthbar = $Camera2D/HealthBar/ProgressBar
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_MusicSlider_value_changed(value):
+	AudioServer.set_bus_volume_db(1, value)
+
+
+func _on_SFXSlider_value_changed(value):
+	AudioServer.set_bus_volume_db(2, value)
