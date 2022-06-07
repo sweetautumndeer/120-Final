17278f74f0420adc87faacd371e9a153ffb14042
sweetautumndeer <amoulios@yahoo.com>
2022-06-06 18:52:31 -0700

    Fixed multiple audio issues.
    Options menu sliders also work on linear amplitude instead of dB now.

--- LevelScenes/Options.gd
+++ LevelScenes/Options.gd
@@ -1,10 +1,7 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+onready var menuHover = $MenuHover
+onready var menuClick = $MenuClick
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,12 +9,21 @@ func _ready():
 
 #changes music volume
 func _on_MusicSlider_value_changed(value):
-	AudioServer.set_bus_volume_db(1, value)
+	var result = 20 * log(value)
+	AudioServer.set_bus_volume_db(1, result)
 
 #changes sfx volume
 func _on_SFXSlider_value_changed(value):
-	AudioServer.set_bus_volume_db(2, value)
+	var result = 20 * log(value)
+	AudioServer.set_bus_volume_db(2, result)
 
 #closes menu when pressed
 func _on_CloseButton_pressed():
+	menuClick.play()
 	visible = false
+
+func _on_CloseButton_mouse_entered():
+	menuHover.play()
+
+func _on_SFXSlider_focus_exited():
+	menuHover.play()
--- LevelScenes/Pause.gd
+++ LevelScenes/Pause.gd
@@ -1,15 +1,11 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+onready var menuHover = $MenuHover
+onready var menuClick = $MenuClick
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -27,14 +23,29 @@ func _process(delta):
 
 #displays options menu
 func _on_OptionsButton_pressed():
+	menuClick.play()
 	$OptionsMenu.visible = true
 
 #pauses screen
 func _on_MainMenuButton_pressed():
+	menuClick.play()
 	get_tree().paused = false
 	get_tree().change_scene("res://LevelScenes/MainMenu.tscn")
 
 #unpauses screen
 func _on_ResumeButton_pressed():
+	menuClick.play()
 	visible = false
 	get_tree().paused = false
+
+
+func _on_ResumeButton_mouse_entered():
+	menuHover.play()
+
+
+func _on_OptionsButton_mouse_entered():
+	menuHover.play()
+
+
+func _on_MainMenuButton_mouse_entered():
+	menuHover.play()
